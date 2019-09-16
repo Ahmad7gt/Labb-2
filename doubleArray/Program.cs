db95c395bd7f82fd70b2b1b77d5e735cc9fc2ae2
Ahmad7gt <55144343+Ahmad7gt@users.noreply.github.com>
2019-09-16 16:27:06 +0200

    Update Program.cs

--- doubleArray/Program.cs
+++ doubleArray/Program.cs
@@ -11,6 +11,40 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+               double[] height = new double[3];
+                height[0] = 0;
+                height[1] = 0;
+                height[2] = 0;
+                
+                //How tall are family members?//
+                Console.WriteLine("How tall is your father?");
+                height[0] = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("How tall is your mother?");
+                height[1] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("How tall is your old sister?");
+                height[2] = double.Parse(Console.ReadLine());
+
+                //Calculate total height and  the average  for family members //
+                double TotalHeight = height[0] + height[1] + height[2];
+                double HeightAverage = height[0] + height[1] + height[2] / 3;
+               
+
+
+                //Write the anserws to the member//
+                Console.WriteLine("The total height of the famille is " + TotalHeight+ ".");
+                Console.WriteLine("Click Enter...");
+                Console.ReadLine();
+                Console.WriteLine("The average height of the famille is" + HeightAverage+".");
+                
+                Console.ReadLine();
+
+
+
+                
+                Console.ReadKey();
 
         }
     }
